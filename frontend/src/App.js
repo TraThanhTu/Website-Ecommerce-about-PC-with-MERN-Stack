4ea63b3bfe08edfe78f098c4116ebab2149c9589
Trà Thanh Tú <tttrathanhtu2002@gmail.com>
2023-09-14 12:45:40 +0700

    slide-header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,38 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductSreen from './screens/ProductScreen';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a href="/">UTH</a>
-      </header>
-      <main>list products</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <div className="slider-header">
+          <div className="slides-header">
+            <input type="radio" name="radio-btn" id="radio1" />
+            <input type="radio" name="radio-btn" id="radio2" />
+            <input type="radio" name="radio-btn" id="radio3" />
+            <div className="slide first">
+              <img src="../images/slide-header1.jpg" alt=""></img>
+            </div>
+            <div className="slide">
+              <img src="../images/slide-header2.jpg" alt=""></img>
+            </div>
+            <div className="slide">
+              <img src="../images/slide-header3.jpg" alt=""></img>
+            </div>
+          </div>
+        </div>
+        
+        <header>
+          <a href="/">UTH</a>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductSreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,4 @@
+function ProductSreen() {
+  return <div>Product Sreen</div>;
+}
+export default ProductSreen;
