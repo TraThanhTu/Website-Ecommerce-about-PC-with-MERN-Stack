98e25c7982c1248dbe9d614dba81934732d00d1f
hiimvanty <hiimvanty@gmail.com>
2023-10-09 14:32:30 +0700

    create sign up screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import { Store } from './Store';
 import CartScreen from './screens/CartScreen'; 
 import SigninScreen from './screens/SigninScreen';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
+import SignupScreen from './screens/SignupScreen';
 
 
 
@@ -99,6 +100,7 @@ function App() {
               {/* Van Ty*/}
               <Route path="/cart" element={<CartScreen />} />
               <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/signup" element={<SignupScreen />} />
               <Route path="/shipping" element={<ShippingAddressScreen />} />
               
               {/*---------*/}
